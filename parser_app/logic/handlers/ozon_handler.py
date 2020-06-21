bce2d0f68ac445629d9b90c69dd8433a0c079047
Alexey Evseev <evseev_alexey94@bk.ru>
2020-06-21 19:59:53 +0300

    fix

--- parser_app/logic/handlers/ozon_handler.py
+++ parser_app/logic/handlers/ozon_handler.py
@@ -232,10 +232,10 @@ class OzonHandler():
         header = {'User-Agent': str(ua.chrome)}
         proxies = None # get_proxy(links_df[links_df.category_id == category_ids[0]].site_link.values[0])
 
-        h1_class = 'b7g0'
-        price_new_class_sale = 'b7s8 b7u0'
-        price_new_class = 'b7s8'
-        price_old_class = 'b7t2'
+        h1_class = 'b8m0'
+        price_new_class_sale = 'b8y7 b9a'
+        price_new_class = 'b8y7'
+        price_old_class = 'b8z1'
         for cat_id in tqdm(category_ids):  # испр
             url_list = links_df[links_df.category_id == cat_id].site_link.values
             category_title = desc_df.loc[cat_id, 'cat_title']
